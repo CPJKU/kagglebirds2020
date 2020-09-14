aaf82a24da8be27f53ed3fe740b0fc7820e67f24
Jan Schlüter <jan.schlueter@jku.at>
2020-09-14 13:43:25 +0200

    Train on longer snippets

--- experiments/train_kagglebirds2020.sh
+++ experiments/train_kagglebirds2020.sh
@@ -419,3 +419,13 @@ for neg_prob in 0.01 0.025 0.05; do
   training="--var float16=1 --var float16.opt_level=O2"
   train 1 vanilla/submedian_rnddownmix_noiseprob10_noisemaxfact10_negprob${neg_prob/./}_groupnorm16_f16 $data $model $metrics $training "$@"
 done
+
+# longer snippets
+for len in 45 60; do
+  data="--var dataset=kagglebirds2020 --var data.downmix=random_uniform --var data.mix_background_noise.probability=1.0 --var data.mix_background_noise.max_factor=1.0 --var data.len_min=$len --var data.len_max=$len"
+  arch="conv2d:64@3x3,groupnorm:16,lrelu,conv2d:64@3x3,groupnorm:16,pool2d:max@3x3,lrelu,conv2d:128@3x3,groupnorm:16,lrelu,conv2d:128@3x3,groupnorm:16,lrelu,conv2d:128@17x3,groupnorm:16,pool2d:max@5x3,lrelu,conv2d:1024@1x9,groupnorm:16,lrelu,dropout:0.5,conv2d:1024@1x1,groupnorm:16,lrelu,dropout:0.5,conv2d:C@1x1"
+  model="--var spect.denoise=submedian --var model.predictor.arch=$arch"
+  metrics=
+  training="--var float16=1 --var float16.opt_level=O2"
+  train 1 vanilla/submedian_rnddownmix_noiseprob10_noisemaxfact10_groupnorm16_len${len}_f16 $data $model $metrics $training "$@"
+done
