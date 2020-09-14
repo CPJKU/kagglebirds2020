a8ca57badc795ea4ed6e077fff42a8fa8b7b7c62
Jan Schlüter <jan.schlueter@jku.at>
2020-09-14 12:19:43 +0200

    Experiment with a proportion of negative (noise-only) examples

--- experiments/train_kagglebirds2020.sh
+++ experiments/train_kagglebirds2020.sh
@@ -409,3 +409,13 @@ for min_freq in 27.5 50 100; do
     train 1 vanilla/submedian_rnddownmix_noiseprob10_noisemaxfact10_groupnorm16_minf${min_freq/./}_maxf${max_freq}_f16 $data $model $metrics $training "$@"
   done
 done
+
+# float16 with median subtraction, downmix augmentation, background noise, groupnorm, negative examples
+for neg_prob in 0.01 0.025 0.05; do
+  data="--var dataset=kagglebirds2020 --var data.downmix=random_uniform --var data.mix_background_noise.probability=1.0 --var data.mix_background_noise.max_factor=1.0 --var data.mix_background_noise.noise_only_probability=$neg_prob"
+  arch="conv2d:64@3x3,groupnorm:16,lrelu,conv2d:64@3x3,groupnorm:16,pool2d:max@3x3,lrelu,conv2d:128@3x3,groupnorm:16,lrelu,conv2d:128@3x3,groupnorm:16,lrelu,conv2d:128@17x3,groupnorm:16,pool2d:max@5x3,lrelu,conv2d:1024@1x9,groupnorm:16,lrelu,dropout:0.5,conv2d:1024@1x1,groupnorm:16,lrelu,dropout:0.5,conv2d:C@1x1"
+  model="--var spect.denoise=submedian --var model.predictor.arch=$arch"
+  metrics=
+  training="--var float16=1 --var float16.opt_level=O2"
+  train 1 vanilla/submedian_rnddownmix_noiseprob10_noisemaxfact10_negprob${neg_prob/./}_groupnorm16_f16 $data $model $metrics $training "$@"
+done
