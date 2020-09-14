5243944af16fc0fd32ff7ae91bf1c9c55de91ef0
Jan Schlüter <jan.schlueter@jku.at>
2020-09-14 18:27:05 +0200

    Experiment with PANN with label smoothing

--- experiments/train_kagglebirds2020.sh
+++ experiments/train_kagglebirds2020.sh
@@ -420,7 +420,7 @@ for neg_prob in 0.01 0.025 0.05; do
   train 1 vanilla/submedian_rnddownmix_noiseprob10_noisemaxfact10_negprob${neg_prob/./}_groupnorm16_f16 $data $model $metrics $training "$@"
 done
 
-# longer snippets
+# float16 with median subtraction, downmix augmentation, background noise, groupnorm, longer snippets
 for len in 45 60; do
   data="--var dataset=kagglebirds2020 --var data.downmix=random_uniform --var data.mix_background_noise.probability=1.0 --var data.mix_background_noise.max_factor=1.0 --var data.len_min=$len --var data.len_max=$len"
   arch="conv2d:64@3x3,groupnorm:16,lrelu,conv2d:64@3x3,groupnorm:16,pool2d:max@3x3,lrelu,conv2d:128@3x3,groupnorm:16,lrelu,conv2d:128@3x3,groupnorm:16,lrelu,conv2d:128@17x3,groupnorm:16,pool2d:max@5x3,lrelu,conv2d:1024@1x9,groupnorm:16,lrelu,dropout:0.5,conv2d:1024@1x1,groupnorm:16,lrelu,dropout:0.5,conv2d:C@1x1"
@@ -428,4 +428,13 @@ for len in 45 60; do
   metrics=
   training="--var float16=1 --var float16.opt_level=O2"
   train 1 vanilla/submedian_rnddownmix_noiseprob10_noisemaxfact10_groupnorm16_len${len}_f16 $data $model $metrics $training "$@"
+done
+
+# float16 pretrained PANN model with downmix augmentation, background noise, log1px, label smoothing
+for alpha in 0.01 0.05; do
+  data="--var dataset=kagglebirds2020 --var data.downmix=random_uniform --var data.mix_background_noise.probability=1.0 --var data.mix_background_noise.max_factor=1.0"
+  model="--var model=pann --var model.num_blocks=$blocks --var magscale.trainable=1"
+  metrics="--var _ce.label_smoothing=$alpha --var _ce.multilabel_dim=1"
+  training="--var float16=1 --var float16.opt_level=O2"
+  train 1 pann/rnddownmix_noiseprob10_noisemaxfact10_blocks6_log1px_lsmooth${alpha/./}_f16 $data $model $metrics $training "$@"
 done
